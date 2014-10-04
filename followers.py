cf0ed4fa85c5c6b602ff0dc88ce8da903e552a25
alb-yeung <alb.yeung@gmail.com>
2014-10-04 11:13:52 -0400

    followers.py

--- followers.py
+++ followers.py
@@ -1,6 +1,6 @@
 from urllib2 import urlopen
 
 website = urlopen("https://api.twitch.tv/kraken/channels/nonamer69xd/follows")
-followers = website.read*()
+followers = website.read()
 
 print followers[1:1]
